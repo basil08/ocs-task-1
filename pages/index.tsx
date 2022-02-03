176e10c33b8219af026c2aaaa2e7e0625677b380
Basil Labib <basillabib01@gmail.com>
2022-02-04 00:41:07 +0530

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,153 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
+
 
 export default function Home() {
+
+  const GITHUB_REST_API_ENDPOINT = "https://api.github.com/";
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [warnings, setWarning] = useState(new Array());
+
+  const [orgName, setOrgName] = useState("");
+  const [repoCount, setRepoCount] = useState(10);
+  const [forkerCount, setForkerCount] = useState(5);
+
+  const [repos, setRepos] = useState([]);
+  // for each "popular" repo, forks will contain an array of its oldest forkers
+  const [forks, setForks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    if (!orgName) {
+      setError("No organization name is set! Please set one to proceed!");
+      // return;
+    }
+
+    if (!repoCount) {
+      const newArr = warnings.slice().push("No repoCount was set! Default value of 10 is assumed!");
+      setWarning("No repoCount was set! Default value of 10 is assumed!");
+      setRepoCount(10);
+    }
+
+    if (!forkerCount) {
+      const newArr = warnings.slice().push("No forkerCount was set! Default value of 5 is assumed!");
+      setWarning("No forkerCount was set! Default value of 5 is assumed!");
+      setForkerCount(5);
+    }
+
+    // make endpoint an .env variable?
+    // but it's client-side request
+    fetch(`${GITHUB_REST_API_ENDPOINT}orgs/${orgName}/repos`, {
+      method: 'GET',
+      headers: { "Content-Type": "application/json" }
+    }).then(async (response) => {
+      if (!response.ok) {
+        // handle errors
+        setError("Response was ill-formed. Please try again!");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    })
+
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Head>
+          <title>Home | OCS Task 1</title>
+        </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <h1 className="text-6xl font-bold">
+            Github Organizations Explorer
+          </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+          <p className="mt-3 text-2xl">
+            Get started by entering <code className="bg-slate-300 rounded font-mono">Organization Name</code>,{' '}
+            <code className="bg-slate-300 rounded font-mono">repoCount</code>, and{' '}
+            <code className="bg-slate-300 rounded font-mono">forkerCount</code>.
+          </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          {error && <p className="p-1 m-1 text-red bg-red-300">{error}</p>}
+          {warnings && <p className="p-1 m-1 text-orange bg-orange-300">{warnings}</p>}
+
+          <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+            <form>
+              <div className="py-1">
+                <label htmlFor="orgname" className="block text-sm font-medium text-black-700">
+                  Organization Name
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    name="orgname"
+                    id="orgname"
+                    onChange={(e) => setOrgName(e.target.value)}
+                    className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                    placeholder="google"
+                    value={orgName}
+                  />
+                </div>
+              </div>
+
+              <div className="py-1">
+                <label htmlFor="n" className="block text-sm font-medium text-black-700">
+                  repoCount
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    name="n"
+                    id="n"
+                    onChange={(e) => setRepoCount(e.target.value)}
+                    className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                    placeholder="10"
+                    value={repoCount}
+                  />
+                </div>
+              </div>
+
+              <div className="py-1">
+                <label htmlFor="m" className="block text-sm font-medium text-black-700">
+                  forkerCount
+                </label>
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <input
+                    type="text"
+                    name="m"
+                    id="m"
+                    onChange={(e) => setForkerCount(e.target.value)}
+                    className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                    placeholder="5"
+                    value={forkerCount}
+                  />
+                </div>
+              </div>
+
+              <div className="py-3">
+                <button
+                  onClick={handleSubmit}
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >Fetch</button>
+              </div>
+            </form>
+          </div>
+        </main>
+
+        <footer className="flex h-24 w-full items-center justify-center border-t">
           <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="flex items-center justify-center hover:text-blue-600"
+            href="https://basil08.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
+            &copy; Basil Labib
           </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
-      </footer>
-    </div>
+        </footer>
+      </div>
   )
 }
